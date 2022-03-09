c5732e2fba78e4e6f3f74884629e1785e474c618
originalAdamTay <94968202+originalAdamTay@users.noreply.github.com>
2022-03-09 22:17:03 +1100

    Added Java files
    Added Driver.java and SplashScreen.java to the repository. Driver.java is the main class used, while SplashScreen.java is used for the splash screen shown on startup of the robot.

---
+++ Main project files/Driver.java
@@ -0,0 +1,29 @@
+import lejos.hardware.Button;
+import lejos.hardware.Sound;
+import lejos.hardware.lcd.LCD;
+
+public class Driver {
+	public void printVersion() {
+		LCD.drawString("V1 - Robotics Group 2", 0, 0);
+		LCD.drawString("Driver class by Adam Tay", 0, 1);
+	}
+	
+	public static void SplashScreen() { // Splash screen method
+		Sound.twoBeeps();
+		Button.LEDPattern(7);
+		LCD.drawString("Bash - version 1", 0, 0);
+		LCD.drawString("alpha (Feb 2022)", 0, 1);
+		LCD.drawString("", 0, 2);
+		LCD.drawString("By Robotics Group", 0, 3);
+		LCD.drawString("2 (Adam Tay,", 0, 4);
+		LCD.drawString("Sam Brown,", 0, 5);
+		LCD.drawString("Bharat Karki and", 0, 6);
+		LCD.drawString("Hiji A.)", 0, 7);
+		Button.ENTER.waitForPressAndRelease();
+	}
+
+	public static void main(String[] args) {
+		SplashScreen(); // Calls splash screen method
+	}
+
+}
