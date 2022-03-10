af64b6d371e21f8f14fe4511d479c3fd5d79743d
hiji <zjac082@live.rhul.ac.uk>
2022-03-10 17:00:35 +0000

    update driver class and add add color feature

---
+++ Behavior classes/CarrotColour.java
@@ -0,0 +1,29 @@
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.robotics.SampleProvider;
+import lejos.robotics.subsumption.Behavior;
+
+public class CarrotColor implements Behavior {
+	EV3ColorSensor cs = new EV3ColorSensor(SensorPort.S1);
+	SampleProvider sp = cs.getAmbientMode();
+	float[] samples = new float[4]; 
+	sp = cs.getRGBMode();
+	sp.fetchSample(samples, 1);
+	
+	public void printVersion() {
+		LCD.drawString("V1 - Robotics Group 2", 0, 0);
+		LCD.drawString("Emergency STOP behavior by hiji", 0, 1);
+	}
+	
+	public boolean takeControl() {
+		sp.fetchSample(samples, 1);
+		//return true if detect orange or green
+		return (samples[1]>0.221) && (samples[1]<0.255f) && (samples[2]>0.124) && (samples[2]<0.178f) && (samples[3]>0.005f) && (samples[3]<0.085f)) ||
+		((samples[1]>0.012f) && (samples[1]<0.163f) && (samples[2]>0.113f) && (samples[2]<0.255f) && (samples[3]>0.000f) && (samples[3]<0.152f));			
+	}
+	
+	public void action() {
+	}
+	
+	public void suppress() {} 
+}
--- Main project files/Driver.java
+++ Main project files/Driver.java
@@ -1,13 +1,18 @@
-import lejos.hardware.Button;
-import lejos.hardware.Sound;
-import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.BaseRegulatedMotor;
+import lejos.hardware.motor.NXTRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.Port;
+import lejos.robotics.chassis.Chassis;
+import lejos.robotics.chassis.Wheel;
+import lejos.robotics.chassis.WheeledChassis;
+import lejos.robotics.navigation.MovePilot;
 import lejos.robotics.subsumption.Arbitrator;
 import lejos.robotics.subsumption.Behavior;
 
 public class Driver {
 	public void printVersion() {
-		LCD.drawString("V1 - Robotics Group 2", 0, 0);
-		LCD.drawString("Driver class by Adam Tay", 0, 1);
+		LCD.drawString("V2 - Robotics Group 2", 0, 0);
+		LCD.drawString("Driver class by Adam tay and hiji", 0, 1);
 	}
 	
 	public static void SplashScreen() { // Splash screen method
@@ -26,11 +31,24 @@ public class Driver {
 
 	public static void main(String[] args) {
 		SplashScreen(); // Calls splash screen method
+		MovePilot pilot = getPilot(MotorPort.A, MotorPort.C, 60, 29);
+		pilot.setLinearSpeed(200);
 		Behavior BatteryLevel = new BatteryLevel(); // Declare behaviours
 		Behavior EmergencyStop = new EmergencyStop();
 		Behavior RabbitSoundsClap = new RabbitSoundsClap(); // TODO: Combine RabbitSounds group of behaviours
 		Behavior RabbitSoundsDark = new RabbitSoundsDark();
 		Behavior RabbitSoundsLight = new RabbitSoundsLight();
-		Arbitrator ab = new Arbitrator(new Behavior[] {RabbitSoundsClap, RabbitSoundsDark, RabbitSoundsLight, EmergencyStop, BatteryLevel}); // Create arbitrator
+		Arbitrator ab = new Arbitrator(new Behavior[] {RabbitSoundsClap, RabbitSoundsDark, RabbitSoundsLight, EmergencyStop, BatteryLevel});
+		ab.go();
+	}
+	
+	private static MovePilot getPilot(Port left, Port right, int diam, int offset) {
+		BaseRegulatedMotor mL = new NXTRegulatedMotor(left);
+		Wheel wL = WheeledChassis.modelWheel(mL, diam).offset(-1 * offset);
+		BaseRegulatedMotor mR = new NXTRegulatedMotor(right);
+		Wheel wR = WheeledChassis.modelWheel(mR, diam).offset(offset);
+		Wheel[] wheels = new Wheel[] {wR, wL};
+		Chassis chassis = new WheeledChassis(wheels, WheeledChassis.TYPE_DIFFERENTIAL);
+		return new MovePilot(chassis);
 	}
 }
