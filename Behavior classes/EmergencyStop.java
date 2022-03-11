185f7bfdecc3598fe2659936442cd9776ca5684c
Hiji <83787341+XLmT09@users.noreply.github.com>
2022-03-11 12:26:50 +0000

    Fix Emergency Stop
    Put a constructor in  emergency stop.

--- Behavior classes/EmergencyStop.java
+++ Behavior classes/EmergencyStop.java
@@ -1,35 +1,28 @@
 import lejos.hardware.Button;
 import lejos.hardware.lcd.LCD;
-import lejos.hardware.port.SensorPort;
-import lejos.hardware.sensor.EV3TouchSensor;
-import lejos.robotics.SampleProvider;
 import lejos.robotics.navigation.MovePilot;
 import lejos.robotics.subsumption.Behavior;
-import lejos.utility.Delay;
 
 public class EmergencyStop implements Behavior {
 	private MovePilot pilot;
-	private EV3TouchSensor ts = new EV3TouchSensor(SensorPort.S3);
-	private SampleProvider sp = ts.getTouchMode();
-	private float[] fetchTouch = new float[1];
-	private int isPressed;
-	private final int FIVE_SECONDS = 5000;
+	
+	public EmergencyStop(MovePilot p) {
+		this.pilot = p;
+	}
 	
 	public void printVersion() {
-		LCD.drawString("V1 - Robotics Group 2", 0, 0);
-		LCD.drawString("Emergency STOP behavior by Adam Tay", 0, 1);
+		LCD.drawString("V2 - Robotics Group 2", 0, 0);
+		LCD.drawString("Emergency STOP behavior by Adam Tay and Hiji", 0, 1);
 	}
 	
 	public boolean takeControl() {
-		sp.fetchSample(fetchTouch, 0);
-		isPressed = (int) fetchTouch[0];
-		return Button.ESCAPE.isDown() || isPressed == 1;
+		return Button.ENTER.isDown();
 	}
 	
 	public void action() {
 		pilot.stop();
-		Delay.msDelay(FIVE_SECONDS);
+		System.exit(0);
 	}
 	
-	public void suppress() {} // suppress method is empty
+	public void suppress() {}
 }
