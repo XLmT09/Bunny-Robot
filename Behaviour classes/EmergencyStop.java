f15bda95e87ca02b622fa4e04b1fb8f17940edb6
Hiji <83787341+XLmT09@users.noreply.github.com>
2022-03-18 11:28:16 +0000

    Update EmergencyStop.java

--- Behaviour classes/EmergencyStop.java
+++ Behaviour classes/EmergencyStop.java
@@ -1,26 +1,18 @@
 import lejos.hardware.Button;
 import lejos.hardware.lcd.LCD;
-import lejos.robotics.navigation.MovePilot;
 import lejos.robotics.subsumption.Behavior;
+import lejos.utility.Delay;
 
-public class EmergencyStop implements Behavior {
-	private MovePilot pilot;
-	
-	public EmergencyStop(MovePilot p) {
-		this.pilot = p;
-	}
-	
-	public void printVersion() {
-		LCD.drawString("V2 - Robotics Group 2", 0, 0);
-		LCD.drawString("Emergency STOP behavior by Adam Tay and Hiji", 0, 1);
-	}
-	
+public class EmergencyStop implements Behavior {	
 	public boolean takeControl() {
 		return Button.ENTER.isDown();
 	}
 	
 	public void action() {
-		pilot.stop();
+		//might need to stop motors
+		LCD.clear();
+		LCD.drawString("Shutting Down", 0, 1);
+		Delay.msDelay(2000);
 		System.exit(0);
 	}
 	
