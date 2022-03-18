52759f80b9623d08bcb2862a491801cf0c51ad06
Hiji <83787341+XLmT09@users.noreply.github.com>
2022-03-18 11:26:19 +0000

    Update LightLevel.java

--- Behaviour classes/LightLevel.java
+++ Behaviour classes/LightLevel.java
@@ -1,42 +1,35 @@
-import lejos.hardware.Button;
-import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.BaseRegulatedMotor;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.robotics.SampleProvider;
-import lejos.robotics.navigation.MovePilot;
 import lejos.robotics.subsumption.Behavior;
 
 public class LightLevel implements Behavior {
-	private MovePilot pilot;
+	private BaseRegulatedMotor mLeft;
+	private BaseRegulatedMotor mRight;
 	private EV3ColorSensor cs;
-	private SampleProvider sp;
-	private float[] lightLevel = new float[1];
+	SampleProvider sp;
+	float[] lightLevel = new float[1];
 	
-	public LightLevel(MovePilot p, EV3ColorSensor cs) {
-		this.pilot = p;
+	public LightLevel(BaseRegulatedMotor left, BaseRegulatedMotor right, EV3ColorSensor cs) {
+		this.mLeft = left;
+		this.mRight = right;
+		this.mLeft.synchronizeWith(new BaseRegulatedMotor[] {this.mRight});
 		this.cs = cs;
 		this.sp = cs.getAmbientMode();
 	}
 	
-	public void printVersion() {
-		LCD.drawString("V1 - Robotics Group 2", 0, 0);
-		LCD.drawString("Light level behaviour by Hiji", 0, 1);
-	}
-	
 	public boolean takeControl() {
 		cs.fetchSample(lightLevel, 0);
-		return lightLevel[0] == 0.00f; // return true if completely dark - for testing purposes only
+		return lightLevel[0] == 0.00f;
 	}
 	
 	public void action() {
-		pilot.setLinearSpeed(100);
-		//Sound.playSample(new File("scared rabbit sound.wav"), Sound.VOL_MAX); // plays wav file at full volume
-		//Button.LEDPattern(5); // red flashing LED light on EV3 brick
-		LCD.drawString("" + lightLevel[0], 0, 3);
-		
+		mLeft.startSynchronization();
+		mLeft.stop();
+		mRight.stop();
+		mLeft.endSynchronization();
 	}
 	
 	public void suppress() {
-		LCD.clear();
-		pilot.setLinearSpeed(180);	
 	}
 }
