ffffd305d1a883e70a17736dea292c45f726112e
Hiji <83787341+XLmT09@users.noreply.github.com>
2022-03-21 16:40:32 +0000

    Update LightLevel.java

--- Behaviour classes/LightLevel.java
+++ Behaviour classes/LightLevel.java
@@ -1,57 +1,58 @@
-import java.io.File;
-
-import lejos.hardware.Button;
-import lejos.hardware.Sound;
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.motor.BaseRegulatedMotor;
-import lejos.hardware.motor.NXTRegulatedMotor;
-import lejos.hardware.port.MotorPort;
-import lejos.hardware.port.SensorPort;
-import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.robotics.SampleProvider;
 import lejos.robotics.subsumption.Behavior;
 
-public class LightLevel implements Behavior {
-	private BaseRegulatedMotor mLeft = new NXTRegulatedMotor(MotorPort.A);
-	private BaseRegulatedMotor mRight = new NXTRegulatedMotor(MotorPort.C);	
-	private EV3ColorSensor cs = new EV3ColorSensor(SensorPort.S3);
-	SampleProvider sp = cs.getRGBMode(); 
-	float[] colLevel = new float[4];
+public class CheckColor implements Behavior {
+	private BaseRegulatedMotor mLeft;
+	private BaseRegulatedMotor mRight;
+	SampleProvider sp;
+	float[] samples = new float[4];
+	private boolean isDark = false;
+	private boolean isGreen = false;
 	
-	public LightLevel() {
-		this.mLeft.synchronizeWith(new BaseRegulatedMotor[] {this.mRight});
-	}
-	
-	public void printVersion() {
-		LCD.drawString("V2 - Robotics Group 2", 0, 0);
-		LCD.drawString("Light level behaviour by Hiji", 0, 1);
-		LCD.drawString("Sound implementation by Adam Tay", 0, 2);
+	public CheckColor(BaseRegulatedMotor left, BaseRegulatedMotor right, SampleProvider sp) {
+		this.mLeft = left;
+		this.mRight = right;
+		this.sp = sp;
 	}
 	
 	public boolean takeControl() {
-		sp.fetchSample(colLevel, 0);
-		//Check if dark using RGB
-		return ((colLevel[1] >= 0.000f) && (colLevel[1] <= 0.063f) && (colLevel[2] >= 0.000f) && 
-				(colLevel[2] <= 0.044f) && (colLevel[3] >= 0.000f) && (colLevel[3] <= 0.043f));
+		sp.fetchSample(samples, 1);
+		//Check if orange using RGB
+		if((samples[1]>0.003f) && (samples[1]<0.031f) && (samples[2]>0.013f) && (samples[2]<0.050f) && (samples[3]>0.003f) && (samples[3]<0.040f))
+			isGreen = true;
+		else if ((samples[1]>0.100f) && (samples[1]<0.160f) && (samples[2]>0.000f) && (samples[2]<0.050f) && (samples[3]>0.000f) && (samples[3]<0.030f))
+			isDark = true;
+		return isGreen || isDark;
 	}
 	
 	public void action() {
-		mLeft.startSynchronization();
-		mLeft.stop();
-		mRight.stop();
-		mLeft.endSynchronization();
-		Sound.setVolume(50); // sets volume to half
-		Sound.playSample(new File("scared rabbit sound.wav")); // plays wav file at half volume
-		Button.LEDPattern(5); // red flashing LED light on EV3 brick
-		LCD.drawString("Too dark!", 0, 0);
+		if(isGreen) {
+			mLeft.setSpeed(180);
+			mRight.setSpeed(180);
+			mLeft.startSynchronization();
+			mLeft.forward();
+			mRight.forward();
+			mLeft.endSynchronization();
+			LCD.drawString("Running towards color", 0, 0);
+		} else if(isDark) {
+			LCD.drawString("Stopping", 0, 0);
+			mLeft.startSynchronization();
+			mLeft.stop();
+			mRight.stop();
+			mLeft.endSynchronization();
+		}
+		isGreen = false;
+		isDark = false;
 	}
 	
 	public void suppress() {
 		LCD.clear();
 		mLeft.startSynchronization();
-		mLeft.setSpeed(180);
-		mRight.setSpeed(180);
-		mLeft.forward();
-		mRight.forward();
-	}
+		mLeft.stop();
+		mRight.stop();
+		mLeft.endSynchronization();
+	} 
+
 }
