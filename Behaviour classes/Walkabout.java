ebdaa6a8091963c4578df1870e288c5a3db5508d
originalAdamTay <94968202+originalAdamTay@users.noreply.github.com>
2022-03-22 00:21:37 +1300

    Update Walkabout.java
    Updated Walkabout.java to remove all references to the MovePilot object and change this to motor synchronisation, also changed version number to reflect major change.

--- Behaviour classes/Walkabout.java
+++ Behaviour classes/Walkabout.java
@@ -3,19 +3,19 @@ import java.io.File;
 import lejos.hardware.Button;
 import lejos.hardware.Sound;
 import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.BaseRegulatedMotor;
+import lejos.hardware.motor.NXTRegulatedMotor;
+import lejos.hardware.port.MotorPort;
 import lejos.robotics.navigation.MovePilot;
 import lejos.robotics.subsumption.Behavior;
 
 public class Walkabout implements Behavior {
-	private MovePilot pilot;
 	private boolean foo = true; // metasyntactic variable
-	
-	Walkabout (MovePilot pilot) {
-		this.pilot = pilot;
-	}
+	private BaseRegulatedMotor mLeft = new NXTRegulatedMotor(MotorPort.A);
+	private BaseRegulatedMotor mRight = new NXTRegulatedMotor(MotorPort.B);
 	
 	public void printVersion() {
-		LCD.drawString("V1 - Robotics Group 2", 0, 0);
+		LCD.drawString("V2 - Robotics Group 2", 0, 0);
 		LCD.drawString("Walkabout behaviour by Adam Tay", 0, 1);
 	}
 	
@@ -24,9 +24,17 @@ public class Walkabout implements Behavior {
 	}
 	
 	public void action() {
-		this.pilot.forward(); // make robot go forwards
+		mLeft.synchronizeWith(new BaseRegulatedMotor[] {mRight}); // synchronise left and right motors
+		mLeft.startSynchronization(); // start synchronisation
+		mLeft.setSpeed(180); // set speed of motors to 180
+		mRight.setSpeed(180);
+		mLeft.forward(); // motors go forward
+		mRight.forward();
 		if (foo == true) { // if carrot detected TODO: implement carrot colour detection system
-			this.pilot.setLinearSpeed(360); // robot increases speed if carrot detected
+			mLeft.setSpeed(360); // double speed
+			mRight.setSpeed(360);
+			mLeft.forward(); // motors go forward
+			mRight.forward();
 			Sound.playSample(new File("happy rabbit sound.wav"), Sound.VOL_MAX); // plays wav file at full volume
 			Button.LEDPattern(4); // green flashing LED light on EV3 brick
 			LCD.drawString("Carrot detected", 0, 0);
@@ -37,6 +45,10 @@ public class Walkabout implements Behavior {
 	}
 	
 	public void suppress() {
-		this.pilot.stop(); // stops robot
+		mLeft.flt(); // set motors to float then stop
+		mRight.flt();
+		mLeft.stop();
+		mRight.stop();
+		mLeft.endSynchronization(); // end synchronisation
 	}
 }
