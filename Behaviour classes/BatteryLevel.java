ca8fa83d58c5fda45b5e9cc6bff4273df4207419
originalAdamTay <94968202+originalAdamTay@users.noreply.github.com>
2022-03-22 00:47:14 +1300

    Update BatteryLevel.java
    Updated BatteryLevel.java to clean up code and remove magic numbers etc.

--- Behaviour classes/BatteryLevel.java
+++ Behaviour classes/BatteryLevel.java
@@ -1,20 +1,30 @@
 import lejos.hardware.Battery;
+import lejos.hardware.Button;
+import lejos.hardware.Sound;
 import lejos.hardware.lcd.LCD;
 import lejos.robotics.subsumption.Behavior;
 import lejos.utility.Delay;
 
 public class BatteryLevel implements Behavior {
+	private final int FIVE_SECONDS = 5000; // eliminate magic numbers
+	
+	public void printVersion() {
+		LCD.drawString("V3.1 - Robotics Group 2", 0, 0);
+		LCD.drawString("Battery level behaviour by Adam Tay and Hiji", 0, 1);
+	}
 	
 	public boolean takeControl() {
-		return Battery.getVoltage() <= 6.0;
+		return Battery.getVoltage() <= 6.0; // take control if battery voltage is less than or equal to 6.0
 	}
 	
 	public void action() {
 		LCD.clear();
+		Sound.beep(); // plays beeping sound
+		Button.LEDPattern(5); // red flashing LED light on EV3 brick
 		LCD.drawString("Battery low!", 0, 0);
-		LCD.drawString("Shutting Down.", 0, 1);
-		Delay.msDelay(5000);
-		System.exit(0);
+		LCD.drawString("Shutting down...", 0, 1);
+		Delay.msDelay(FIVE_SECONDS);
+		System.exit(0); // exit program with state 0
 	}
 	
 	public void suppress() {
