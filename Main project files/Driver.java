bd6c138f339acabfe53d0e57d5a4c7f48789e800
originalAdamTay <94968202+originalAdamTay@users.noreply.github.com>
2022-03-22 04:07:26 +1100

    Update Driver.java
    Updated Driver.java to retain calibration method but also add CheckColor behaviour declaration/add CheckColor behaviour to arbitrator object.

--- Main project files/Driver.java
+++ Main project files/Driver.java
@@ -1,13 +1,15 @@
 import lejos.hardware.Button;
 import lejos.hardware.Sound;
 import lejos.hardware.lcd.LCD;
+import lejos.robotics.SampleProvider;
 import lejos.robotics.subsumption.Arbitrator;
 import lejos.robotics.subsumption.Behavior;
 
 public class Driver {
 	public void printVersion() {
-		LCD.drawString("V5 - Robotics Group 2", 0, 0);
+		LCD.drawString("V6 - Robotics Group 2", 0, 0);
 		LCD.drawString("Driver class by Adam Tay", 0, 1);
+		LCD.drawString("Calibration method by Bharat Karki", 0, 2);
 	}
 	
 	public static void SplashScreen() { // Splash screen method
@@ -31,23 +33,22 @@ public class Driver {
 		
 		while (Button.ENTER.isUp()) {
 			soundSampleProvider.fetchSample(maxSoundSample, 0);
-			
 			if (maxSoundSample[0] > maxSoundLevel) {
 				maxSoundLevel = maxSoundSample[0];
 			}
 		}
-		
 		return maxSoundLevel;
 	}
 
 	public static void main(String[] args) {
 		SplashScreen(); // Calls splash screen method
 		Behavior BatteryLevel = new BatteryLevel(); // Declare behaviours
+		Behavior CheckColor = new CheckColor(null, null, null); // TODO: Sort out arguments for CheckColor
 		Behavior EmergencyStop = new EmergencyStop();
 		Behavior RabbitSounds = new RabbitSounds();
 		Behavior RabbitSoundsTableTrundle = new RabbitSoundsTableTrundle();
-		Behavior Walkabout = new Walkabout(null); // TODO: Implement MovePilot object into Walkabout behaviour as argument
-		Arbitrator ab = new Arbitrator(new Behavior[] {Walkabout, RabbitSounds, RabbitSoundsTableTrundle, EmergencyStop, BatteryLevel}); // Create arbitrator
+		Behavior Walkabout = new Walkabout();
+		Arbitrator ab = new Arbitrator(new Behavior[] {Walkabout, RabbitSounds, RabbitSoundsTableTrundle, CheckColor, EmergencyStop, BatteryLevel}); // Create arbitrator
 		ab.go();
 	}
 }
