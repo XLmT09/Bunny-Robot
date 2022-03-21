9bb172ef892c298df82308e9c687345cb91cc311
originalAdamTay <94968202+originalAdamTay@users.noreply.github.com>
2022-03-22 00:02:56 +1300

    Update Driver.java
    Updated Driver.java to remove all references to MovePilot objects and changed version number as this is a major change.

--- Main project files/Driver.java
+++ Main project files/Driver.java
@@ -1,20 +1,12 @@
 import lejos.hardware.Button;
 import lejos.hardware.Sound;
 import lejos.hardware.lcd.LCD;
-import lejos.hardware.motor.BaseRegulatedMotor;
-import lejos.hardware.motor.NXTRegulatedMotor;
-import lejos.hardware.port.MotorPort;
-import lejos.hardware.port.Port;
-import lejos.robotics.chassis.Chassis;
-import lejos.robotics.chassis.Wheel;
-import lejos.robotics.chassis.WheeledChassis;
-import lejos.robotics.navigation.MovePilot;
 import lejos.robotics.subsumption.Arbitrator;
 import lejos.robotics.subsumption.Behavior;
 
 public class Driver {
 	public void printVersion() {
-		LCD.drawString("V4 - Robotics Group 2", 0, 0);
+		LCD.drawString("V5 - Robotics Group 2", 0, 0);
 		LCD.drawString("Driver class by Adam Tay", 0, 1);
 	}
 	
@@ -32,21 +24,9 @@ public class Driver {
 		Button.ENTER.waitForPressAndRelease();
 		LCD.clear();
 	}
-	
-	private static MovePilot getPilot(Port left, Port right, int diam, int offset) {
-		BaseRegulatedMotor mL = new NXTRegulatedMotor(left);
-		Wheel wL = WheeledChassis.modelWheel(mL, diam).offset(-1 * offset);
-		BaseRegulatedMotor mR = new NXTRegulatedMotor(right);
-		Wheel wR = WheeledChassis.modelWheel(mR, diam).offset(offset);
-		Wheel[] wheels = new Wheel[] {wR, wL};
-		Chassis chassis = new WheeledChassis(wheels, WheeledChassis.TYPE_DIFFERENTIAL);
-		return new MovePilot(chassis);
-	}
 
 	public static void main(String[] args) {
 		SplashScreen(); // Calls splash screen method
-		MovePilot pilot = getPilot(MotorPort.A, MotorPort.C, 60, 29);
-		pilot.setLinearSpeed(180);
 		Behavior BatteryLevel = new BatteryLevel(); // Declare behaviours
 		Behavior EmergencyStop = new EmergencyStop();
 		Behavior RabbitSounds = new RabbitSounds();
