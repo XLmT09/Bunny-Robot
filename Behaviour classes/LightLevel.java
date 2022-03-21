3521d7faa935ff164125d3ec58b2c92a85874ad3
originalAdamTay <94968202+originalAdamTay@users.noreply.github.com>
2022-03-21 23:55:45 +1100

    Update LightLevel.java
    Updated LightLevel.java to add sound implementation.

--- Behaviour classes/LightLevel.java
+++ Behaviour classes/LightLevel.java
@@ -1,40 +1,8 @@
-import lejos.hardware.motor.BaseRegulatedMotor;
-import lejos.hardware.sensor.EV3ColorSensor;
-import lejos.robotics.SampleProvider;
-import lejos.robotics.subsumption.Behavior;
+import java.io.File;
 
-public class LightLevel implements Behavior {
-	private BaseRegulatedMotor mLeft;
-	private BaseRegulatedMotor mRight;
-	private EV3ColorSensor cs;
-	SampleProvider sp;
-	float[] lightLevel = new float[1];
-	
-	public LightLevel(BaseRegulatedMotor left, BaseRegulatedMotor right, EV3ColorSensor cs) {
-		this.mLeft = left;
-		this.mRight = right;
-		this.mLeft.synchronizeWith(new BaseRegulatedMotor[] {this.mRight});
-		this.cs = cs;
-		this.sp = cs.getAmbientMode();
-	}
-	
-	public boolean takeControl() {
-		cs.fetchSample(lightLevel, 0);
-		return lightLevel[0] == 0.00f;
-	}
-	
-	public void action() {
-		mLeft.startSynchronization();
-		mLeft.stop();
-		mRight.stop();
-		mLeft.endSynchronization();
-	}
-	
-	public void suppress() {
-	}
-}
-
-//new programme to test
+import lejos.hardware.Button;
+import lejos.hardware.Sound;
+import lejos.hardware.lcd.LCD;
 import lejos.hardware.motor.BaseRegulatedMotor;
 import lejos.hardware.motor.NXTRegulatedMotor;
 import lejos.hardware.port.MotorPort;
@@ -54,6 +22,12 @@ public class LightLevel implements Behavior {
 		this.mLeft.synchronizeWith(new BaseRegulatedMotor[] {this.mRight});
 	}
 	
+	public void printVersion() {
+		LCD.drawString("V2 - Robotics Group 2", 0, 0);
+		LCD.drawString("Light level behaviour by Hiji", 0, 1);
+		LCD.drawString("Sound implementation by Adam Tay", 0, 2);
+	}
+	
 	public boolean takeControl() {
 		sp.fetchSample(colLevel, 0);
 		//Check if dark using RGB
@@ -66,8 +40,18 @@ public class LightLevel implements Behavior {
 		mLeft.stop();
 		mRight.stop();
 		mLeft.endSynchronization();
+		Sound.setVolume(50); // sets volume to half
+		Sound.playSample(new File("scared rabbit sound.wav")); // plays wav file at half volume
+		Button.LEDPattern(5); // red flashing LED light on EV3 brick
+		LCD.drawString("Too dark!", 0, 0);
 	}
 	
 	public void suppress() {
+		LCD.clear();
+		mLeft.startSynchronization();
+		mLeft.setSpeed(180);
+		mRight.setSpeed(180);
+		mLeft.forward();
+		mRight.forward();
 	}
 }
