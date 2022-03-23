3b21f135da5f3a239b77476831f2521c364ecf36
originalAdamTay <94968202+originalAdamTay@users.noreply.github.com>
2022-03-23 22:29:46 +1100

    Update CheckColor.java
    Updated CheckColor.java to implement sound files for too dark.

--- Behaviour classes/CheckColor.java
+++ Behaviour classes/CheckColor.java
@@ -4,80 +4,74 @@ import lejos.hardware.Button;
 import lejos.hardware.Sound;
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.motor.BaseRegulatedMotor;
+import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.robotics.SampleProvider;
 import lejos.robotics.subsumption.Behavior;
+import lejos.utility.Delay;
 
 public class CheckColor implements Behavior {
 	private BaseRegulatedMotor mLeft;
 	private BaseRegulatedMotor mRight;
+	private BaseRegulatedMotor head;
+	private EV3ColorSensor cs;
 	SampleProvider sp;
 	float[] samples = new float[4];
 	private boolean isDark = false;
-	private boolean isGreen = false;
+	private boolean isGreenOrange = false;
+	final static int HEAD_ROTATION= -120;
+	final static int HEAD_ROTATION_CALIB = 180;
 	
-	public CheckColor(BaseRegulatedMotor left, BaseRegulatedMotor right, SampleProvider sp) {
+	public CheckColor(BaseRegulatedMotor left, BaseRegulatedMotor right, EV3ColorSensor cs, BaseRegulatedMotor head) {
 		this.mLeft = left;
 		this.mRight = right;
-		this.sp = sp;
+		this.head = head;
+		this.cs = cs;
+		this.sp = cs.getRGBMode();
 	}
 	
 	public void printVersion() {
-		LCD.drawString("V1 - Robotics Group 2", 0, 0);
+		LCD.drawString("V2 - Robotics Group 2", 0, 0);
 		LCD.drawString("Carrot detection and light level program by Hiji", 0, 1);
-		LCD.drawString("Sound files and Walkabout behaviour implementation by Adam Tay", 0, 2);
+		LCD.drawString("Sound files implementation by Adam Tay", 0, 2);
 	}
 	
 	public boolean takeControl() {
 		sp.fetchSample(samples, 1);
-		// Check if green detected using RGB
-		if ((samples[1]>0.003f) && (samples[1]<0.031f) && (samples[2]>0.013f) && (samples[2]<0.050f) && (samples[3]>0.003f) && (samples[3]<0.040f)) {
-			isGreen = true;
-		}
-		// Use RGB to check if too dark
-		else if ((samples[1]>0.100f) && (samples[1]<0.160f) && (samples[2]>0.000f) && (samples[2]<0.050f) && (samples[3]>0.000f) && (samples[3]<0.030f)) {
+		//Check if orange using RGB
+		if (((samples[1]>0.003f) && (samples[1]<0.031f) && (samples[2]>0.013f) && (samples[2]<0.050f) && (samples[3]>0.003f) && (samples[3]<0.040f)) ||
+				((samples[1]>0.030f) && (samples[1]<0.095f) && (samples[2]>0.005f) && (samples[2]<0.023f) && (samples[3]>0.008f) && (samples[3]<0.019f)))
+			isGreenOrange = true;
+		else if ((samples[1]>0.100f) && (samples[1]<0.160f) && (samples[2]>0.000f) && (samples[2]<0.050f) && (samples[3]>0.000f) && (samples[3]<0.030f))
 			isDark = true;
-		}
-		return isGreen || isDark; // if green detected or too dark return true
+		return isGreenOrange || isDark;
 	}
 	
 	public void action() {
-		mLeft.synchronizeWith(new BaseRegulatedMotor[] {mRight}); // synchronise left and right motors
-		mLeft.startSynchronization(); // start synchronisation
-		mLeft.setSpeed(180); // set speed of motors to 180
-		mRight.setSpeed(180);
-		mLeft.forward(); // motors go forward
-		mRight.forward();
-		if (isGreen) { // if green detected - carrot detected
-			mLeft.setSpeed(360); // double speed
-			mRight.setSpeed(360);
-			mLeft.forward(); // motors go forward
-			mRight.forward();
-			Sound.playSample(new File("happy rabbit sound.wav"), Sound.VOL_MAX); // plays wav file at full volume
-			Button.LEDPattern(4); // green flashing LED light on EV3 brick
-			LCD.drawString("Carrot detected", 0, 0);
-			LCD.drawString("nearby!", 0, 1);
-		} 
-		else if(isDark) {
-			mLeft.stop();
-			mRight.stop();
-			mLeft.endSynchronization();
+		LCD.drawString("stopping", 0, 0);
+		mLeft.startSynchronization();
+		mLeft.stop();
+		mRight.stop();
+		mLeft.endSynchronization();
+		
+		if (isDark) {
+			head.rotate(HEAD_ROTATION_CALIB);
+			mLeft.rotate(180, false);
+			mRight.rotate(360, false);
+			head.rotate(HEAD_ROTATION);
 			Sound.setVolume(50); // sets volume to half
-			Sound.playSample(new File("scared rabbit sound.wav"));
+			Sound.playSample(new File("rabbit scared sound.wav")); // plays wav file at half volume
 			Button.LEDPattern(5); // red flashing LED light on EV3 brick
-		}
-		Button.ENTER.waitForPressAndRelease();
-		LCD.clear();
-		isGreen = false;
+			LCD.clear();
+			LCD.drawString("Too dark!", 0, 0);
+			Button.ENTER.waitForPressAndRelease();
+			LCD.clear();
+		} 
+		isGreenOrange = false;
 		isDark = false;
 	}
 	
 	public void suppress() {
-		LCD.clear(); // clear LCD screen
-		mLeft.flt(); // set motors to float then stop
-		mRight.flt();
-		mLeft.stop();
-		mRight.stop();
-		mLeft.endSynchronization(); // end synchronisation
+		LCD.clear();
 	} 
 
 }
