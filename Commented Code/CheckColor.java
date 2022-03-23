5bb5cc125595633cba7f65b7475e8297db31774d
thelamasialad <95089976+thelamasialad@users.noreply.github.com>
2022-03-23 14:10:54 +0000

    Add files via upload

---
+++ Commented Code/CheckColor.java
@@ -0,0 +1,67 @@
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.BaseRegulatedMotor;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.robotics.SampleProvider;
+import lejos.robotics.subsumption.Behavior;
+import lejos.utility.Delay;
+
+public class CheckColor implements Behavior {
+	private BaseRegulatedMotor mLeft;
+	private BaseRegulatedMotor mRight;
+	private BaseRegulatedMotor head; //Linear motor for head movements
+	private EV3ColorSensor cs;
+	SampleProvider sp;
+	float[] samples = new float[4];
+	private boolean isDark = false;
+	private boolean isGreenOrange = false;
+	final static int HEAD_ROTATION= -120;
+	final static int HEAD_ROTATION_CALIB = 180;
+	
+	public CheckColor(BaseRegulatedMotor left, BaseRegulatedMotor right, EV3ColorSensor cs, BaseRegulatedMotor head) {
+		this.mLeft = left;
+		this.mRight = right;
+		this.head = head;
+		this.cs = cs;
+
+		//sample provider set to RGB mode
+		this.sp = cs.getRGBMode();
+	}
+	
+	public boolean takeControl() {
+		sp.fetchSample(samples, 1);
+		//Check for orange using RGB values
+		if (((samples[1]>0.003f) && (samples[1]<0.031f) && (samples[2]>0.013f) && (samples[2]<0.050f) && (samples[3]>0.003f) && (samples[3]<0.040f)) ||
+				//Check for green using RGB values
+				((samples[1]>0.030f) && (samples[1]<0.095f) && (samples[2]>0.005f) && (samples[2]<0.023f) && (samples[3]>0.008f) && (samples[3]<0.019f)))
+			isGreenOrange = true;
+		//Check for dark environment using RGB values
+		else if ((samples[1]>0.100f) && (samples[1]<0.160f) && (samples[2]>0.000f) && (samples[2]<0.050f) && (samples[3]>0.000f) && (samples[3]<0.030f))
+			isDark = true;
+		return isGreenOrange || isDark;
+	}
+	
+	public void action() {
+		LCD.drawString("stopping", 0, 0);
+		mLeft.startSynchronization();
+		mLeft.stop();
+		mRight.stop();
+		mLeft.endSynchronization();
+
+		//Startled rabbit movements
+		if(isDark) {
+			head.rotate(HEAD_ROTATION_CALIB);
+			mLeft.rotate(180, false);
+			mRight.rotate(360, false);
+			head.rotate(HEAD_ROTATION); //Rabbit head movements
+		} 
+
+		//Revert values
+		isGreenOrange = false;
+		isDark = false;
+	}
+	
+	public void suppress() {
+		LCD.clear();
+	} 
+
+}
---
+++ Commented Code/EmergencyStop.java
@@ -0,0 +1,19 @@
+import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
+import lejos.robotics.subsumption.Behavior;
+import lejos.utility.Delay;
+
+public class EmergencyStop implements Behavior {	
+	public boolean takeControl() {
+		return Button.ENTER.isDown(); //returns if enter button is pressed
+	}
+	
+	public void action() {
+		LCD.clear();
+		LCD.drawString("Shutting Down", 0, 1);
+		Delay.msDelay(1000); //Delay for message to persist on LCD
+		System.exit(0); //End program
+	}
+	
+	public void suppress() {}
+}
