453a1c453ba3ef92c9c3960ef5aebb38c3cfe964
SamBrown2021 <95089775+SamBrown2021@users.noreply.github.com>
2022-03-23 20:12:30 +0000

    Update EmergencyStop.java

--- Final submission/Final Jack-Rabbit Code/EmergencyStop.java
+++ Final submission/Final Jack-Rabbit Code/EmergencyStop.java
@@ -4,6 +4,8 @@ import lejos.robotics.subsumption.Behavior;
 import lejos.utility.Delay;
 
 public class EmergencyStop implements Behavior {	
+	private static int ONE_SECOND = 1000;
+	
 	public boolean takeControl() {
 		return Button.ENTER.isDown(); //returns if enter button is pressed
 	}
@@ -11,7 +13,7 @@ public class EmergencyStop implements Behavior {
 	public void action() {
 		LCD.clear();
 		LCD.drawString("Shutting Down", 1, 2);
-		Delay.msDelay(1000); //Delay for message to persist on LCD
+		Delay.msDelay(ONE_SECOND); //Delay for message to persist on LCD
 		System.exit(0); //End program
 	}
 	
